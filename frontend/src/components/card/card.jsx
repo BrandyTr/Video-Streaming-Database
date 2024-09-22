87eb6a6cb54103ee005ed91d9561e2f59b4018e8
hà thục anh <hathucanh@hohihehe.local>
2024-09-22 23:43:38 +0700

    Feature: Description + TrailerButton on Hover

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './card.css';
+import React from "react";
+import "./card.css";
 
 const Card = ({ title, description, thumbnail, onClick }) => (
   <div className="card" onClick={onClick}>
-    <img className='thumbnail' src={thumbnail} alt={title} />
-    <h2>{title}</h2>
-    <p>{description}</p>
+    <img className="thumbnail" src={thumbnail} alt={title} />
+    <div className="overlay"></div>
+    <div className="details">
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <button className="trailer-button">Watch trailer</button>
+    </div>
   </div>
 );
-
 
 export default Card;
