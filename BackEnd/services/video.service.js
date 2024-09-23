90f840069e6e8b7041d33648cc09b9101b4b350b
qthais <thaiallb63@gmail.com>
2024-09-23 11:59:23 +0700

    modified service

--- BackEnd/services/video.service.js
+++ BackEnd/services/video.service.js
@@ -1,13 +1,11 @@
 const Video = require("../models/video.model")
 
-exports.createVideo=async(id, name, key, site, type, official, published_at)=>{
+exports.createVideo=async(id, name, key, site, published_at)=>{
     const video= new Video({
         id,
         name,
         key,
         site,
-        type,
-        official,
         published_at:new Date(published_at)
     })
     try{
