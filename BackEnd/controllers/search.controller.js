440e6f7eca44e134612e09f6d01ba5e3f5969991
baochammm <lebaotram9612@gmail.com>
2024-10-01 21:34:01 +0700

    Your commit message

--- BackEnd/controllers/search.controller.js
+++ BackEnd/controllers/search.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model")
 const { fetchFromTMDB } = require("../services/tmdb.service")
 const Person = require('../models/person.model')
+const Movie = require('../models/movie.model')
 class SearchController {
     async searchPerson(req, res) {
         const name =req.params.query
@@ -17,7 +18,18 @@ class SearchController {
         }
     }
     async searchMovie(req, res) {
-
+        const title = req.params.query
+        if (!title) {
+            return res.status(400).json({ success: false, message: "No title provided!" })
+        }
+        try {
+            const movies = await Movie.find({
+                title: { $regex: title, $options: 'i' }
+            })
+            res.json({ success: true, content: movies });
+        } catch (err) {
+            res.status(400).json({ success: false, message: err.message });
+        }
     }
     async searchTv(req, res) {
 
