c45a35e7dfb68b19bfbf69f03868392fca44d5a4
hà thục anh <hathucanh@hohihehe.local>
2024-10-12 12:53:52 +0700

    render video link

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './card.css';
+import React from "react";
+import "./card.css";
+import { Link } from "react-router-dom";
 
-const Card = ({ title, description, thumbnail, onClick }) => (
-  <div className="card" onClick={onClick}>
-    <img className='thumbnail' src={thumbnail} alt={title} />
-    <h2>{title}</h2>
-    <p>{description}</p>
+const Card = ({ title, id, description, thumbnail }) => (
+  <div className="card">
+    <Link to={`/watching/${id}`}>
+      <img className="thumbnail" src={thumbnail} alt={title} />
+      <div className="overlay"></div>
+      <div className="details">
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+        <button className="trailer-button ">Watch trailer</button>
+      </div>
+    </Link>
   </div>
 );
-
-
 export default Card;
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500";
+// lay poster cua phim cho card
