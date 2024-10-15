d83a82115986293794cc3ecda25a2d18cf6c5bb6
Brandy Tran <baotutran1103@gmail.com>
2024-10-15 10:14:07 +0700

    Revert "Merge pull request #17 from BrandyTr/rendervideo"
    This reverts commit d0a53ac0fa1aa4054bc6a9f254cc028049335323, reversing
    changes made to 5f09276c5c3bc1549d523e131eab4bb3f83efdf2.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar, Footer } from "./commonPaths";
-import Homepage from "./pages/home/homepage";
-import Watching from "./pages/watching/watching";
-import Notfound from "./pages/notfound/notfound";
+import { Navbar, Footer} from './commonPaths'
+import Homepage from './pages/home/homepage';
+import Watching from './pages/watching/watching';
+import Notfound from './pages/notfound/notfound';
 
-import "./App.css";
+import './App.css'
+
 
 function App() {
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/watching/:id" element={<Watching />} />
+        <Route path="/watching" element={<Watching />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
       <Footer />
     </Router>
-  );
+  )
 }
-export default App;
+
+export default App
