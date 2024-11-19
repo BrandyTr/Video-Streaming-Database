d1611ecb92265036dbb5bfec2e3c33a572f29936
hà thục anh <hathucanh@hohihehe.local>
2024-11-19 13:28:25 +0700

    h

--- frontend/src/pages/detail/cast.jsx
+++ frontend/src/pages/detail/cast.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { CastCard } from "../../commonPaths"; // Adjust the import path as needed
 import "./cast.css";
+import "swiper/css";
+import { Navigation, Virtual } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 const Cast = ({ credit }) => {
   //useEffect n useState to get array of cast from credit
   const [cast, setCast] = useState([]);
@@ -16,13 +19,25 @@ const Cast = ({ credit }) => {
     <div className="frame">
       <h1>Cast</h1>
       <div className="Casts">
-        {cast.map((member, index) => (
-          <CastCard
-            key={index}
-            name={member.name}
-            profile_path={member.profile_path}
-          />
-        ))}
+        {/* Swiper for cast */}
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={8}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          virtual // Enable virtual slides
+          modules={[Virtual]} // Add the Virtual module
+        >
+          {cast.map((member, index) => (
+            <SwiperSlide key={index}>
+              <CastCard
+                key={index}
+                name={member.name}
+                profile_path={member.profile_path}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
