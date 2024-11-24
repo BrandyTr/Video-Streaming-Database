97630eecc39e93e1469a60a8d973c7c3add78bbc
hà thục anh <hathucanh@hohihehe.local>
2024-11-24 18:23:51 +0700

    added card scores

--- frontend/src/components-main/movie-card/MovieCard.jsx
+++ frontend/src/components-main/movie-card/MovieCard.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Button from "../button/Button";
 import movieApi from "../../api/movieApi";
 import { image_API } from "../../api/apiConfig";
+import { FaUser } from "react-icons/fa";
 
 import "./movie-card.css";
 
@@ -12,7 +13,7 @@ const MovieCard = (props) => {
 
   const item = props.item;
 
-  const link = `/watching/${item.id}`;
+  const link = `/detail/${item.id}`;
 
   const bg = image_API.originalImage(item.backdrop_path || item.poster_path);
 
@@ -24,8 +25,8 @@ const MovieCard = (props) => {
         if (response.data.content.genres) {
           const fetchedGenres = response.data.content.genres; // Ensure this is the correct path
           setGenres(fetchedGenres); // Update state with fetched data
-          // console.log("Fetched data:");
-          // console.log(response.data.content.genres);
+          console.log("Fetched data:");
+          console.log(response.data.content.genres);
         }
         // console.log("Fetched data:", response.data.content);
       } catch (error) {
@@ -46,9 +47,24 @@ const MovieCard = (props) => {
         {/* <Button>
                     <i className="">hi</i>
                 </Button> */}
+
+        <div className="movie-rating-box">
+          <p>{item.averageRating}</p>
+        </div>
+
         <div className="title-box-wrapper">
           <h3>{item.title || item.name}</h3>
           <div className="movie-info-hover">
+            <div className="card-scores">
+              <div className="rating-box-hover">
+                <p>({item.averageRating})</p>
+              </div>
+              <p>|</p>
+              <div className="card-views">
+                <p>{item.ratingCount}</p>
+                <FaUser></FaUser>
+              </div>
+            </div>
             <p>{genreNames}</p>
             {/* <p>{genres.map(genre => genre.name).join(' • ')}</p> */}
           </div>
