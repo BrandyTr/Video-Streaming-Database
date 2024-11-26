7461a7675635e2c7b1711a731e1e2b9528d45e1f
qthais <thaiallb63@gmail.com>
2024-11-26 21:49:48 +0700

    admin route

---
+++ frontend/src/pages/home/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components-main/header/Header";
+function AdminPage() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+      </div>
+      <div>AdminPage</div>
+    </>
+  );
+}
+
+export default AdminPage;
--- frontend/src/pages/home/HomeScreenCheck.jsx
+++ frontend/src/pages/home/HomeScreenCheck.jsx
@@ -1,10 +1,16 @@
 
 
 import { useAuth } from '../../Context/authContext';
+import AdminPage from './AdminPage';
 import AuthScreen from './AuthScreen';
 import HomePage_Main from './HomePage-main';
 const HomeScreenCheck = () => {
   const {user}=useAuth()
+  if(user?.role=='admin'){
+    return (
+      <AdminPage/>
+    )
+  }
   return (
     <>
       {user?<HomePage_Main/>:<AuthScreen/>}
